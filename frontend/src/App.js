555b542c43d0b54e72f440d2c59e59101c1fba0a
ayoub10adnane <ayoub.ikamilwa@gmail.com>
2024-11-29 19:48:55 +0100

    Test: Adding comment to test feature branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import TaskInput from './components/TaskInput';
 import axios from 'axios';
 import Plant from './components/Plant';
 
-//new comment brotha
+//you have a new update
 function App() {
   const [tasks, setTasks] = useState([]);
   const [report, setReport] = useState(null);
