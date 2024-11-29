cc6bbd733918782ff0de310bd3ac12167f717e87
ayoub10adnane <ayoub.ikamilwa@gmail.com>
2024-11-29 19:20:46 +0100

    Fix Jest tests and configurations

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom', // For frontend testing
-  };
-  
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '\\.(gif|png|jpe?g|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+};
